a500450209a5b799c4d1a3f7338714643acc344a
Joey A <66380080+joey-ammar@users.noreply.github.com>
2021-06-02 08:25:07 +0200

    CI

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,7 +2,7 @@ import axios from "axios";
 require("dotenv").config();
 
 export default axios.create({
-  baseURL: `Http://api.unsplash.com`,
+  baseURL: `Https://api.unsplash.com`,
   headers: {
     Authorization: `Client-ID ${process.env.REACT_APP_KEY}`,
   },
